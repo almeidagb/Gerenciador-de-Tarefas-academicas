84301b0ce28cba5b97a933406df577af6f856599
gabrieleaparecidaalmeida34@gmail.com <gabrieleaparecidaalmeida34@gmail.com>
2026-04-06 21:40:49 -0300

    Adicionado api e filtro de status de tarefa

--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from .models import Tarefa
+
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all().values()
+    return JsonResponse(list(tarefas), safe=False)
+
+def listar_tarefas_por_status(request,status):
+    status_validos = [choice[0] for choice in Tarefa.STATUS_CHOICES]
+    
+    if status not in status_validos:
+        return JsonResponse({'erro': 'status inválidos'}, status=400)
+    
+    tarefas = Tarefa.objects.filter(status=status).values()
+    return JsonResponse(list[Any](tarefas), safe=False)
